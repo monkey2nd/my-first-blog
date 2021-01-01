cae4c9f4141c618f246577aa4ea3f919328bc62e
firlambda <saitokoki1216@yahoo.co.jp>
2021-01-02 00:48:15 +0900

    Added views to create editforms clog post inside the site

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
+from django import forms
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .froms import PostForm
+from django.shortcuts import redirect
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -11,3 +14,32 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request,'blog/post_edit.html',{'form':form})
+
+def post_edit(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST,instance=post)
+        # フォームを保存するとき
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.publiished_date = timezone.now()
+            post.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+        # このポストを編集するためにただフォームを開く場合
+    return render(request,'blog/post_edit.html',{'form':form})
